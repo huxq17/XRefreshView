4be2a279e8071723fea9a3621e5c0f2c9bd7edbb
huxq17 <huxiaoqian@2144.cn>
2016-06-08 19:59:39 +0800

    去掉XRefreshViewFooter中onStateReady中的show(true)

--- library/src/main/java/com/andview/refreshview/XRefreshViewFooter.java
+++ library/src/main/java/com/andview/refreshview/XRefreshViewFooter.java
@@ -50,7 +50,7 @@ public class XRefreshViewFooter extends LinearLayout implements IFooterCallBack 
         mHintView.setVisibility(View.GONE);
         mProgressBar.setVisibility(View.GONE);
         mClickView.setVisibility(View.VISIBLE);
-        show(true);
+//        show(true);
     }
 
     @Override
