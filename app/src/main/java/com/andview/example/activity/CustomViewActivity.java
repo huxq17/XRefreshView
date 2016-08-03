0413742f171b51c832b49eebbae17ce95a4eae4a
huxq17 <huxq17@163.com>
2016-08-03 17:10:41 +0800

    完善自定义view的上拉下拉判断条件的设定

--- app/src/main/java/com/andview/example/activity/CustomViewActivity.java
+++ app/src/main/java/com/andview/example/activity/CustomViewActivity.java
@@ -21,100 +21,104 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CustomViewActivity extends Activity {
-	private StickyListHeadersListView stickyLv;
-	private List<StickyListBean> list = new ArrayList<StickyListBean>();
-	private XRefreshView refreshView;
-	private int mTotalItemCount;
-	private StickylistAdapter adapter;
+    private StickyListHeadersListView stickyLv;
+    private List<StickyListBean> list = new ArrayList<StickyListBean>();
+    private XRefreshView refreshView;
+    private int mTotalItemCount;
+    private StickylistAdapter adapter;
 
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_customview);
-		initData();
-		stickyLv = (StickyListHeadersListView) findViewById(R.id.sticky_list);
-		adapter = new StickylistAdapter(getApplicationContext(), list);
-		stickyLv.setAdapter(adapter);
-		refreshView = (XRefreshView) findViewById(R.id.custom_view);
-		refreshView.setPullLoadEnable(true);
-		refreshView.setAutoRefresh(true);
-//		refreshView.setPinnedTime(0);
-		refreshView.setCustomHeaderView(new CustomHeader(this));
-		refreshView.setCustomFooterView(new CustomerFooter(this));
-		refreshView.setOnTopRefreshTime(new OnTopRefreshTime() {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customview);
+        initData();
+        stickyLv = (StickyListHeadersListView) findViewById(R.id.sticky_list);
+        adapter = new StickylistAdapter(getApplicationContext(), list);
+        stickyLv.setAdapter(adapter);
+        refreshView = (XRefreshView) findViewById(R.id.custom_view);
+        refreshView.setPullLoadEnable(true);
+        refreshView.setAutoRefresh(true);
+//        refreshView.setPinnedTime(0);
+        refreshView.setCustomHeaderView(new CustomHeader(this));
+        refreshView.setCustomFooterView(new CustomerFooter(this));
+        refreshView.setOnTopRefreshTime(new OnTopRefreshTime() {
 
-			@Override
-			public boolean isTop() {
-				return stickyLv.getFirstVisiblePosition() == 0;
-			}
-		});
-		refreshView.setOnBottomLoadMoreTime(new OnBottomLoadMoreTime() {
+            @Override
+            public boolean isTop() {
+                if (stickyLv.getFirstVisiblePosition() == 0) {
+                    View firstVisibleChild = stickyLv.getListChildAt(0);
+                    return firstVisibleChild.getTop() >= 0;
+                }
+                return false;
+            }
+        });
+        refreshView.setOnBottomLoadMoreTime(new OnBottomLoadMoreTime() {
 
-			@Override
-			public boolean isBottom() {
-				if(stickyLv.getLastVisiblePosition() == mTotalItemCount - 1){
-					View item = stickyLv.getChildAt(stickyLv.getLastVisiblePosition());
+            @Override
+            public boolean isBottom() {
+                if (stickyLv.getLastVisiblePosition() == mTotalItemCount - 1) {
+                    View lastChild = stickyLv.getListChildAt(stickyLv.getListChildCount() - 1);
+                    return (lastChild.getBottom() + stickyLv.getPaddingBottom()) <= stickyLv.getMeasuredHeight();
+                }
+                return false;
+            }
+        });
+        stickyLv.setOnScrollListener(new OnScrollListener() {
 
-				}
-				return stickyLv.getLastVisiblePosition() == mTotalItemCount - 1;
-			}
-		});
-		stickyLv.setOnScrollListener(new OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
 
-			@Override
-			public void onScrollStateChanged(AbsListView view, int scrollState) {
+            }
 
-			}
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem,
+                                 int visibleItemCount, int totalItemCount) {
+                mTotalItemCount = totalItemCount;
+            }
+        });
+        refreshView.setXRefreshViewListener(new XRefreshView.SimpleXRefreshListener() {
 
-			@Override
-			public void onScroll(AbsListView view, int firstVisibleItem,
-					int visibleItemCount, int totalItemCount) {
-				mTotalItemCount = totalItemCount;
-			}
-		});
-		refreshView.setXRefreshViewListener(new XRefreshView.SimpleXRefreshListener() {
+            @Override
+            public void onRefresh() {
 
-			@Override
-			public void onRefresh() {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        refreshView.stopRefresh();
+                    }
+                }, 2000);
+            }
 
-				new Handler().postDelayed(new Runnable() {
-					@Override
-					public void run() {
-						refreshView.stopRefresh();
-					}
-				}, 5000);
-			}
+            @Override
+            public void onLoadMore(boolean isSlience) {
 
-			@Override
-			public void onLoadMore(boolean isSlience) {
+                new Handler().postDelayed(new Runnable() {
 
-				new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        refreshView.stopLoadMore();
+                    }
+                }, 2000);
+            }
+        });
+    }
 
-					@Override
-					public void run() {
-						refreshView.stopLoadMore();
-					}
-				}, 2000);
-			}
-		});
-	}
+    int section = 0;
+    String YM = null;
+    String content = null;
 
-	int section = 0;
-	String YM = null;
-	String content = null;
+    private void initData() {
 
-	private void initData() {
+        for (int i = 0; i < 20; i++) {
+            if (i % 5 == 0) {
+                section++;
+                YM = "第" + section + "个头";
+            }
+            content = "第" + i + "项数据";
+            StickyListBean bean = new StickyListBean(section, YM, content);
+            list.add(bean);
+        }
 
-		for (int i = 0; i < 20; i++) {
-			if (i % 5 == 0) {
-				section++;
-				YM = "第" + section + "个头";
-			}
-			content = "第" + i + "项数据";
-			StickyListBean bean = new StickyListBean(section, YM, content);
-			list.add(bean);
-		}
-
-	}
+    }
 }
